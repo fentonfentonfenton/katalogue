29a26d8c8b5d4016baf0ee24a89cd100beca8e49
jfbookingbug <fenton@fenton.home>
2019-09-24 21:18:19 +0100

    make work

--- kill.sh
+++ kill.sh
@@ -1,5 +1,10 @@
 rm -rf redis/
 rm -rf pgdata/
+rm -rf whiskerboard/
 docker volume ls -f dangling=true
 docker system prune --volumes -f
 docker images -a | grep "s" | awk '{print $3}' | xargs docker rmi --force
+mkdir redis
+mkdir pgdata
+mkdir whiskerboard
+sleep 10
